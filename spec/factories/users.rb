b2fcb8f2740a22c31173481473ab37a2a64188bf
CodeJJ422 <n23ao102@gmail.com>
2025-06-04 15:30:17 +0900

    ユーザー管理機能の実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'a00000'}
+    password_confirmation {password}
+    last_name             {'ふりま'}
+    first_name            {'たろう'}
+    last_name_kana        {'フリマ'}
+    first_name_kana       {'タロウ'}
+    birth_date            {'2000-04-03'}
+  end
+end
