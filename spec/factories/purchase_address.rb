b5a0273372fe8403d2b09acf4a93fc92d84931fb
CodeJJ422 <n23ao102@gmail.com>
2025-06-14 18:38:51 +0900

    Formオブジェクトを用いて複数のテーブルに情報を保存

---
+++ spec/factories/purchase_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_address do
+    postal_code    {'111-1101'}
+    prefecture_id  {'41'}
+    city           {'福岡市'}
+    street_address {'西区'}
+    building_name  {'マンションA'}
+    phone_number   {'12345678901'}
+  end
+end
