6360db5c01e98c9fb68ae0ca67e93b72336f8a06
CodeJJ422 <n23ao102@gmail.com>
2025-06-17 20:51:17 +0900

    javascriptでtokenをコントローラーに送信

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -19,7 +19,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
 end
